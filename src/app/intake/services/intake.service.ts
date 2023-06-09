35a27b4e7cb6b867897571ea996d7ffb84494596
g00glen00b <dimitri.mestdagh@optis.be>
2023-06-10 01:20:35 +0200

    fix: improved IntakeService.findTotalIntakeDosesUntil() performance

--- src/app/intake/services/intake.service.ts
+++ src/app/intake/services/intake.service.ts
@@ -1,22 +1,9 @@
 import {Injectable} from '@angular/core';
 import {ScheduleService} from "../../schedule/services/schedule.service";
-import {
-  combineLatest, defaultIfEmpty,
-  filter,
-  from,
-  generate,
-  groupBy,
-  map,
-  mergeMap,
-  Observable,
-  reduce,
-  take,
-  tap,
-  toArray
-} from "rxjs";
+import {defaultIfEmpty, filter, from, groupBy, map, mergeMap, Observable, reduce, take, tap, toArray} from "rxjs";
 import {Intake} from "../models/intake";
 import {Schedule} from "../../schedule/models/schedule";
-import {add, addDays, differenceInDays, Duration, isAfter, isBefore, isEqual, isSameDay, parse, set} from "date-fns";
+import {differenceInDays, isAfter, isBefore, isEqual, parse, set} from "date-fns";
 import {ScheduleRecurrence} from "../../schedule/models/schedule-recurrence";
 import {compareByField} from "../../shared/utils/compare-utils";
 import {MIDNIGHT} from "../../shared/utils/date-fns-utils";
@@ -26,8 +13,6 @@ import {CompletedIntake} from "../models/completed-intake";
 import {TotalIntakeDose} from "../models/total-intake-dose";
 import {MediminderEventService} from "../../shared/services/mediminder-event.service";
 import {IntakeCompletedEvent} from "../models/intake-completed-event";
-import {da} from "date-fns/locale";
-import {ScheduleRecurrenceType} from "../../schedule/models/schedule-recurrence-type";
 
 const STORE_NAME = 'intake';
 
@@ -67,22 +52,33 @@ export class IntakeService {
   }
 
   findTotalIntakeDosesUntil(date: Date): Observable<TotalIntakeDose[]> {
-    const todayAtMidnight: Date = set(new Date(), MIDNIGHT);
     const dateAtMidnight: Date = set(date, MIDNIGHT);
-    return generate({
-      initialState: todayAtMidnight,
-      condition: date => !isAfter(date, dateAtMidnight),
-      iterate: date => addDays(date, 1)
-    }).pipe(
-      mergeMap(date => this.findByDate(date)),
-      mergeMap(intakes => from(intakes)),
-      groupBy(intake => intake.schedule.medication.id),
-      mergeMap(group => group
-        .pipe(
-          map(intake => intake.schedule.dose),
-          reduce((totalDose, dose) => totalDose + dose, 0),
+    return this.scheduleService
+      .findAll({field: 'name', direction: 'asc'})
+      .pipe(
+        mergeMap(schedules => from(schedules)),
+        map(schedule => this.mapScheduleToIntakeDose(schedule, dateAtMidnight)),
+        groupBy(total => total.medicationId),
+        mergeMap(group => group.pipe(
+          map(group => group.totalDose),
+          reduce((result, dose) => result + dose, 0),
           map(totalDose => ({totalDose, medicationId: group.key})))),
-      toArray());
+        filter(total => total.totalDose > 0),
+        toArray());
+  }
+
+  private mapScheduleToIntakeDose(schedule: Schedule, date: Date): TotalIntakeDose {
+    const {medication: {id: medicationId}, recurrence, period: {startingAt, endingAtInclusive}} = schedule;
+    const todayAtMidnight: Date = set(new Date(), MIDNIGHT);
+    const startDate = isBefore(startingAt, todayAtMidnight) ? todayAtMidnight : startingAt;
+    const endDate = endingAtInclusive == null || isBefore(date, endingAtInclusive) ? date : endingAtInclusive;
+    const days = differenceInDays(endDate, startingAt);
+    const alreadyPassedDays = differenceInDays(startDate, startingAt);
+    if (days <= 0) return {medicationId, totalDose: 0};
+    const expectedDays = this.calculateExpectedDaysInRecurrence(recurrence);
+    const totalDose = Math.floor(days / expectedDays) * schedule.dose;
+    const alreadyPassedDose = Math.floor(alreadyPassedDays / expectedDays) * schedule.dose;
+    return {medicationId, totalDose: totalDose - alreadyPassedDose};
   }
 
   private mapScheduleToIntake(schedule: Schedule, date: Date): Observable<Intake> {
@@ -102,27 +98,6 @@ export class IntakeService {
     return this.dbService
       .getAllByIndex<CompletedIntakeEntity>(STORE_NAME, 'scheduleId', IDBKeyRange.only(schedule.id))
       .pipe(take(1));
-  }
-
-  private findAllIntakeDatesUntil(schedule: Schedule, givenDate: Date): Observable<Date> {
-    const startDateAtScheduleTime: Date = parse(schedule.time, 'HH:mm', schedule.period.startingAt);
-    const givenDateAtScheduleTime: Date = parse(schedule.time, 'HH:mm', givenDate);
-    const endDateAtScheduleTime: Date | null = schedule.period.endingAtInclusive == null ? null : parse(schedule.time, 'HH:mm', schedule.period.endingAtInclusive);
-    const actualEndDate: Date = endDateAtScheduleTime == null || isBefore(givenDateAtScheduleTime, endDateAtScheduleTime) ? givenDateAtScheduleTime : endDateAtScheduleTime;
-    const duration: Duration = this.calculateDuration(schedule.recurrence);
-
-    return generate({
-      initialState: startDateAtScheduleTime,
-      condition: date => !isAfter(date, actualEndDate),
-      iterate: date => add(date, duration)
-    });
-  }
-
-  private calculateDuration(recurrence: ScheduleRecurrence): Duration {
-    switch (recurrence.type) {
-      case 'daily': return {days: recurrence.units};
-      case 'weekly': return {weeks: recurrence.units};
-    }
   }
 
   private isActiveAtDate(schedule: Schedule, date: Date): boolean {
