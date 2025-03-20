e9e6bac78e52ad995efd1ea73ae20796db38eada
g00glen00b <dimitri.mestdagh@optis.be>
2025-03-20 23:00:01 +0100

    chore: removed unused import

--- mediminder-api/src/main/java/codes/dimitri/mediminder/api/user/implementation/UserController.java
+++ mediminder-api/src/main/java/codes/dimitri/mediminder/api/user/implementation/UserController.java
@@ -11,7 +11,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
