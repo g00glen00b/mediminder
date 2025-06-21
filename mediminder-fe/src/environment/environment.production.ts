ef15da6842d0f255724cb19637da4eed16747cfb
g00glen00b <4865705+g00glen00b@users.noreply.github.com.>
2025-06-21 22:53:57 +0200

    fix: fixed production environment

--- mediminder-fe/src/environment/environment.production.ts
+++ mediminder-fe/src/environment/environment.production.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  logoutHandler: 'https://dev-vmcdsx7rgptuj26g.us.auth0.com/v2/logout?client_id=4KZ1MpQoRQpb7YZrYVF8U91pcajlS2HV',
-  loginHandler: '/oauth2/authorization/auth0',
-  apiUrl: '../api'
+  auth0Domain: 'dev-vmcdsx7rgptuj26g.us.auth0.com',
+  auth0ClientId: '4KZ1MpQoRQpb7YZrYVF8U91pcajlS2HV',
+  apiUrl: 'https://mediminder-m5xo.onrender.com/api',
+  authoritiesClaim: 'https://mediminder.app/roles',
+  audience: 'https://api.mediminder.app',
 };
